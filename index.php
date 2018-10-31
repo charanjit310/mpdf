71bf864355607f4b3f6128aa1e96d638c2084e47
charanjit <m>
2018-11-01 00:41:12 +0530

    new coment

--- index.php
+++ index.php
@@ -9,13 +9,14 @@ require_once __DIR__ . '/vendor/autoload.php';
     $title = $_POST['title'];
     $link  = $_POST['link'];
 
-
-
     ob_start();
+
     require 'moviePdfHtml.php';
     $htmlFile = ob_get_clean();
     ob_clean();
 
     $mpdf = new \Mpdf\Mpdf();
+
     $mpdf->WriteHTML(utf8_encode($htmlFile));
+
     $mpdf->Output('MyPDF.pdf', 'D');
